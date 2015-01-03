dc092258c1d5e36f971602c8a66209ffdad0d3f5
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-01-03 19:46:18 +0100

    Removed old friends controller
    #BS-25 Comment

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -2,10 +2,6 @@
 
 angular.module('BudgetSupervisor.controllers', [])
 
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
 .controller('LoginController', function () {
 })
 
