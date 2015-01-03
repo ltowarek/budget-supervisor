19fd1b9f7d28410f84e062e95e64c8daf2a7ebcc
Lukasz Towarek <lukasz.towarek@gmail.com>
2015-01-03 19:51:44 +0100

    Added CategoriesController test
    #BS-25 Comment

--- test/spec/controllers.js
+++ test/spec/controllers.js
@@ -1,25 +1,64 @@
 'use strict';
 
-describe('Controller: PetIndexCtrl', function () {
+describe('Controller: CategoriesController', function () {
 
-  var should = chai.should();
-
-  // load the controller's module
   beforeEach(module('BudgetSupervisor'));
 
-  var FriendsCtrl,
+  var categoriesController,
+    categoriesService,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector) {
     scope = $rootScope.$new();
-    FriendsCtrl = $controller('FriendsCtrl', {
+    categoriesService = $injector.get('CategoriesService');
+    categoriesController = $controller('CategoriesController', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of friends to the scope', function () {
-    scope.friends.should.have.length(4);
+  it('should attach a list of categories to the scope', function () {
+    expect(scope.categories, 'categories').to.have.length(3);
   });
 
+  it('should attach a list of config values to the scope', function () {
+    expect(scope.config, 'config').to.have.property('showDelete', false);
+    expect(scope.config, 'config').to.have.property('showReorder', false);
+  });
+
+  it('should toggle showDelete value and disable showReorder', function () {
+    scope.toggleDelete();
+
+    expect(scope.config.showDelete, 'showDelete').equal(true);
+    expect(scope.config.showReorder, 'showReorder').equal(false);
+  });
+
+  it('should toggle showReorder value and disable showDelete', function () {
+    scope.toggleReorder();
+
+    expect(scope.config.showReorder, 'showReorder').equal(true);
+    expect(scope.config.showDelete, 'showDelete').equal(false);
+  });
+
+  it('should call service remove function', function () {
+    var spy = sinon.spy(categoriesService, 'remove');
+
+    scope.remove(0);
+
+    expect(spy.callCount, 'remove function call counts').to.equal(1);
+    expect(spy.args[0][0], 'removed category id').to.equal(0);
+    expect(scope.config.showDelete, 'showDelete').equal(false);
+  });
+
+  it('should call service reorder function', function () {
+    var spy = sinon.spy(categoriesService, 'reorder');
+
+    scope.reorder({}, 0, 1);
+
+    expect(spy.callCount, 'reorder function call counts').to.equal(1);
+    expect(spy.args[0][0], 'reordered category object').to.eql({});
+    expect(spy.args[0][1], 'reordered category from index').to.equal(0);
+    expect(spy.args[0][2], 'reordered category to index').to.equal(1);
+    expect(scope.config.showDelete, 'showDelete').equal(false);
+  });
 });
