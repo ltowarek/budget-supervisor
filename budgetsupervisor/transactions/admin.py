668188dca48f7bed9137f7c9dc479427c85b3f22
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-06-27 22:36:23 +0200

    Create transaction model

--- budgetsupervisor/transactions/admin.py
+++ budgetsupervisor/transactions/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payee, Category, Transaction
+
+admin.site.register((Payee, Category, Transaction))
--- budgetsupervisor/transactions/models.py
+++ budgetsupervisor/transactions/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+            verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
+
+
+class Payee(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Transaction(models.Model):
+    class TransactionType(models.TextChoices):
+        WITHDRAWAL = 'W', _('Withdrawal')
+        DEPOSIT = 'D', _('Deposit')
+        TRANSFER = 'T', _('Transfer')
+
+    name = models.CharField(max_length=200)
+    date = models.DateField('transaction date')
+    transaction_type = models.CharField(max_length=1, choices=TransactionType.choices, default=TransactionType.WITHDRAWAL)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    payee = models.ForeignKey(Payee, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
