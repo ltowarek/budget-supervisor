2aa829bd67b90d3b183c6c86efa087d01db4224d
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-06-28 13:38:01 +0200

    Add model views

--- budgetsupervisor/transactions/urls.py
+++ budgetsupervisor/transactions/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = 'transactions'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.TransactionListView.as_view(), name='transaction_list'),
+    path('create/', views.TransactionCreate.as_view(), name='transaction_create'),
+    path('<int:pk>/update', views.TransactionUpdate.as_view(), name='transaction_update'),
+    path('<int:pk>/delete', views.TransactionDelete.as_view(), name='transaction_delete'),
+    path('category/', views.CategoryListView.as_view(), name='category_list'),
+    path('category/create/', views.CategoryCreate.as_view(), name='category_create'),
+    path('category/<int:pk>/update', views.CategoryUpdate.as_view(), name='category_update'),
+    path('category/<int:pk>/delete', views.CategoryDelete.as_view(), name='category_delete'),
 ]
--- budgetsupervisor/transactions/views.py
+++ budgetsupervisor/transactions/views.py
@@ -1,5 +1,46 @@
-from django.http import HttpResponse
+from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Category, Transaction
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class TransactionListView(generic.ListView):
+    def get_queryset(self):
+        return Transaction.objects.all()
+
+
+class TransactionCreate(CreateView):
+    model = Transaction
+    fields = '__all__'
+
+
+class TransactionUpdate(UpdateView):
+    model = Transaction
+    fields = '__all__'
+
+
+class TransactionDelete(DeleteView):
+    model = Transaction
+    success_url = reverse_lazy('transactions')
+
+
+class CategoryListView(generic.ListView):
+    def get_queryset(self):
+        return Category.objects.all()
+
+
+class CategoryCreate(CreateView):
+    model = Category
+    fields = '__all__'
+
+
+class CategoryUpdate(UpdateView):
+    model = Category
+    fields = '__all__'
+
+
+class CategoryDelete(DeleteView):
+    model = Category
+    success_url = reverse_lazy('transactions/category')
+
