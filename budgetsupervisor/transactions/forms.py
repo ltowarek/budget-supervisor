dbe2b199937937845e775b9bb62a2d3594d70719
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-07-16 22:14:59 +0200

    Remove redundant check

--- budgetsupervisor/transactions/forms.py
+++ budgetsupervisor/transactions/forms.py
@@ -14,13 +14,8 @@ class ImportTransactionsForm(forms.Form):
         response = app.get(url)
         data = response.json()
 
-        already_imported_ids = Transaction.objects.filter(external_id__isnull=False).values_list("external_id", flat=True)
-
         for imported_transaction in data['data']:
             imported_id = int(imported_transaction['id'])
-
-            if imported_id in already_imported_ids:
-                continue
 
             escaped_category = imported_transaction["category"].replace("_", " ")
             category = Category.objects.filter(name__iexact=escaped_category)
