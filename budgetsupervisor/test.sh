ad57f47e6d9d49c3abad79cb4e3d580da9264b74
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-09-15 08:57:24 +0200

    Configure pytest and coverage

--- budgetsupervisor/test.sh
+++ budgetsupervisor/test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pytest --cov=. --no-cov-on-fail --cov-report html -vv
+pytest
--- budgetsupervisor/unit_test.sh
+++ budgetsupervisor/unit_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pytest -m "not webtest" --cov=. --no-cov-on-fail --cov-report html -vv
+pytest -m "not webtest"
