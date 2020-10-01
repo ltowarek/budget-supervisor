b8e8e0f52822d58b913e9730260c7febdc2dbce6
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-10-01 22:41:45 +0200

    Update TODO comment

--- budgetsupervisor/budget/models.py
+++ budgetsupervisor/budget/models.py
@@ -43,7 +43,7 @@ class ConnectionManager(models.Manager):
 
 class Connection(models.Model):
     provider = models.CharField(max_length=200, editable=False)
-    # TODO: Can external_id be set to null?
+    # TODO: Can external_id be set to null? Connection without external_id makes no sense.
     external_id = models.BigIntegerField(blank=True, null=True, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
