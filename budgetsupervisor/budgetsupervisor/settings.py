ec078997aedbed6bab02fe70f75bc0bf356682e6
Lukasz Towarek <lukasz.towarek@gmail.com>
2020-11-08 21:44:51 +0100

    Handle static files in debug mode

--- budgetsupervisor/budgetsupervisor/settings.py
+++ budgetsupervisor/budgetsupervisor/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+if DEBUG:
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 LOGIN_URL = "/users/login/"
 LOGIN_REDIRECT_URL = "/"
